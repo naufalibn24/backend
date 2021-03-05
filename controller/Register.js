9671953c563da3c90066370533f8d6956fbfc815
naufalibn24 <naufalibnu75@gmail.com>
2021-03-05 13:30:42 +0700

    Backend Ecommerce

--- controller/Register.js
+++ controller/Register.js
@@ -1,41 +1,40 @@
-const User = require('../model/User')
-const bcrypt = require ('bcryptjs');
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 // const {check, validationResult} = require ('express-validator');
-class RegisterController{
-    static Register (req, res){
-        // const errors = validationResult (req);
-        // if (!errors.isEmpty ()) {
-        //   return res.status (400).json ({errors: errors.array ()});
-        // }
-        bcrypt.hash (req.body.password, saltRounds, function (err, hash) {
-          console.log('reg', req.body)
-            const {username,email, namaDepan, umur,namaBelakang } = req.body
-          User.create ({
-            username : username,
-            password : hash,
-            email : email,
-            namaDepan : namaDepan,
-            namaBelakang : namaBelakang,
-            umur : umur,
-          })
+class RegisterController {
+  static Register(req, res) {
+    // const errors = validationResult (req);
+    // if (!errors.isEmpty ()) {
+    //   return res.status (400).json ({errors: errors.array ()});
+    // }
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+      const { username, email, namaDepan, umur, namaBelakang } = req.body;
+      User.create({
+        username: username,
+        password: hash,
+        email: email,
+        namaDepan: namaDepan,
+        namaBelakang: namaBelakang,
+        umur: umur,
+      })
 
-            .then (result => {
-              res.status (201).json ({
-                success: true,
-                msg: 'Success Create',
-                data: result,
-              });
-            })
-            .catch (err => {
-              res.status (500).json ({
-                success: false,
-                msg: 'Failed Register',
-                detail: err,
-              });
-            });
+        .then((result) => {
+          res.status(201).json({
+            success: true,
+            msg: "Success Create",
+            data: result,
+          });
+        })
+        .catch((err) => {
+          res.status(500).json({
+            success: false,
+            msg: "Failed Register",
+            detail: err,
+          });
         });
-    }
+    });
+  }
 }
 
-module.exports = RegisterController
+module.exports = RegisterController;
--- route/RegisterRoute.js
+++ route/RegisterRoute.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const RegisterController = require('../controller/Register.js');
-const {check, validationResult} = require ('express-validator');
+const { check, validationResult } = require('express-validator');
+
 router.post('/api/user/register', RegisterController.Register)
 
 module.exports = router;
